7435b581f065fa75ce1ff700998b7af9194dea9f
stevensrf11 <stevensrf@mail.com>
2022-08-27 16:33:59 -0500

    Add files via upload

---
+++ Super.User.Resources/Constants/SuperUserModuleNames.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Super.User.Resources.Constants
+{
+    /// <summary>
+    /// SuperUserModuleNames class
+    /// Contains  a set of module names  
+    /// </summary>
+    public static class SuperUserModuleNames
+    {
+        /// <summary>
+        /// SuperUserLogModule field
+        /// The name of the log module.
+        /// </summary>
+        /// <value>const string</value>
+        public const string ModuleLog = "SuperUserLogModule";
+
+        /// <summary>
+        /// ModuleSuperUser field
+        /// The name of module navigation bar modul.
+        /// </summary>
+        /// <value>const string</value>
+        public const string ModuleSuperUser = "SuperUserModule";
+
+        /// <summary>
+        /// The name of module Recording
+        /// </summary>
+        public const string ModuleRecording = "SuperUserRecordingModule";
+
+        /// <summary>
+        /// The name of module Status.
+        /// </summary>
+        public const string ModuleStatus = "SuperUserStatusModule";
+
+        /// <summary>
+        /// The name of module Scripting.
+        /// </summary>
+        public const string ModuleScripting = "SuperUserScriptingModule";
+
+        /// <summary>
+        /// The name of module Render.
+        /// </summary>
+        public const string ModuleRender = "SuperUserRenderModule";
+
+        /// <summary>
+        /// The name of module Magnetics
+        /// </summary>
+        public const string ModuleMagnetics = "SuperUserMagneticsModule";
+
+        /// <summary>
+        /// The name of module PostBist
+        /// </summary>
+        public const string ModulePostBist= "SuperUserPostBistModule";
+
+        /// <summary>
+        /// The name of module Functional
+        /// </summary>
+        public const string ModuleFunctional = "SuperUserFunctionalModule";
+
+        /// <summary>
+        /// The name of module Functional
+        /// </summary>
+        public const string ModuleOohThings = "SuperUserOohThingsModule";
+
+
+        public const string ModuleStatusCollection = "SuperUserStatusCollectionModule";
+
+        public const string ModuleStatusFrontPanel = "SuperUseStatusFrontPanelModule";
+
+        public const string ModuleStatusBoard= "SuperUserStatusBoardModule";
+
+
+        public const string ModuleRenderCatheter = "SuperUserRenderCatheterModule";
+
+
+
+    }
+
+}
